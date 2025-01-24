512d6c0123ba7a74b932f2cc188734a0e61b0544
Volodymyr B <volodymyr.b@didww.com>
2025-01-24 11:09:40 +0200

    use only phoneNumber handleType

--- ios/Classes/CallKitController.swift
+++ ios/Classes/CallKitController.swift
@@ -65,7 +65,7 @@ class CallKitController : NSObject {
         providerConfiguration.supportsVideo = false
         providerConfiguration.maximumCallsPerCallGroup = 1
         providerConfiguration.maximumCallGroups = 1;
-        providerConfiguration.supportedHandleTypes = [.generic, .phoneNumber]
+        providerConfiguration.supportedHandleTypes = [.phoneNumber] //.generic,
         
         if #available(iOS 11.0, *) {
             providerConfiguration.includesCallsInRecents = false
